be739279852aa7e608f7c3e7922bbe182bce21da
ayushshriwastav <shriwastavayush22@gmail.com>
2025-07-25 16:51:47 +0530

    firl of check even odd and prime number

---
+++ evenoddprimenum.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    int choice, count;
+    int result = 0, number;
+
+    printf("Select operation\n");
+    printf("1. Check Even and Odd Number \n");
+    printf("2. Check Prime Number \n");
+    printf("Choose your choice : ");
+    scanf("%d", &choice);
+
+    if (choice < 1){
+        printf("Invalid Input! Please atleast one number...\n");
+        return 1;
+    }
+    
+    printf("How much you want enter the number : ");
+    scanf("%d", &count);
+
+    for (int i = 1; i<= count;i++){
+        printf("Enter the number : ",i + 1 );
+        scanf("%d", &number);
+
+        switch(choice){
+            case 1:
+            if (number % 2 == 0){
+             printf("%d is even number \n", number);  
+            }
+            else {
+                printf("%d is odd number \n", number);
+            }
+            break;
+            case 2:
+            break;
+            default :
+            break;
+        }
+    }
+    return 0;
+}
